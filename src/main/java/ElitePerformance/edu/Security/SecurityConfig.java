04572a2607c2818988e55154e960d58606d2062b
Ridwan2024-P <ahsanrid@gmail.com>
2026-05-15 20:28:00 +0600

    Security Update

---
+++ src/main/java/ElitePerformance/edu/Security/SecurityConfig.java
@@ -0,0 +1,62 @@
+package ElitePerformance.edu.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/api/performances/calculate")
+                        .hasRole("MANAGER")
+
+                        .requestMatchers("/api/employee/**")
+                        .hasRole("EMPLOYEE")
+
+                        .requestMatchers("/api/admin/**")
+                        .hasRole("ADMIN")
+
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+
+        UserDetails employee = User.builder()
+                .username("emp")
+                .password("{noop}1234")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails manager = User.builder()
+                .username("manager")
+                .password("{noop}1234")
+                .roles("MANAGER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1234")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(employee, manager, admin);
+    }
+}
